08a5e184634508f4674579a3cb3c0a849c3d1fac
AyaBengherifa <aya.bengherifa@4d.com>
2024-01-15 17:00:20 +0100

    Table ds

---
+++ src/components/InputTable/InputTable.settings.ts
@@ -0,0 +1,28 @@
+import { ESetting, TSetting } from '@ws-ui/webform-editor';
+import { BASIC_SETTINGS, DEFAULT_SETTINGS, load } from '@ws-ui/webform-editor';
+
+const commonSettings: TSetting[] = [
+  {
+    key: 'name',
+    label: 'Name',
+    type: ESetting.TEXT_FIELD,
+    defaultValue: 'Qodly',
+  },
+];
+
+const Settings: TSetting[] = [
+  {
+    key: 'properties',
+    label: 'Properties',
+    type: ESetting.GROUP,
+    components: commonSettings,
+  },
+  ...DEFAULT_SETTINGS,
+];
+
+export const BasicSettings: TSetting[] = [
+  ...commonSettings,
+  ...load(BASIC_SETTINGS).filter('style.overflow'),
+];
+
+export default Settings;
